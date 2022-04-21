01208984ebe7824ab9bfba704562484e7d4cfa84
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-04-21 11:28:16 +0700

    add unique in table items at name

--- db/migrate/20220421041010_create_admins.rb
+++ db/migrate/20220421041010_create_admins.rb
@@ -3,7 +3,7 @@ class CreateAdmins < ActiveRecord::Migration[7.0]
     create_table :admins do |t|
       t.string :id
       t.string :name
-      t.string :email
+      t.string :email , unique: true
       t.string :password
       t.string :role
 
