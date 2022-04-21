d83334d16e7971f9458d47989af977d17bcf7cda
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-04-21 23:05:51 +0700

    fixing views and controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to _item_url(@item), notice: "Item was successfully created." }
+        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to _item_url(@item), notice: "Item was successfully updated." }
+        format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
     @item.destroy
 
     respond_to do |format|
-      format.html { redirect_to _items_url, notice: "Item was successfully destroyed." }
+      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -65,6 +65,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :proce, :image, :description)
+      params.require(:item).permit(:name, :price, :image, :description)
     end
 end
--- app/views/items/_item.json.jbuilder
+++ app/views/items/_item.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! item, :id, :name, :proce, :image, :description, :created_at, :updated_at
+json.extract! item, :id, :name, :price, :image, :description, :created_at, :updated_at
 json.url item_url(item, format: :json)
