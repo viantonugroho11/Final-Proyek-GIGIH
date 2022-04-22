7cbe4266008ad129f4dc8b59a69890c59e4c1f9a
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-04-22 07:59:07 +0700

    add Javascript and Boostrap

--- Gemfile
+++ Gemfile
@@ -3,8 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.3"
 
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.1"
+
+gem 'bootstrap', '~> 4.5.2'
+gem 'jquery-rails'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- db/migrate/20220419170534_create_items.rb
+++ db/migrate/20220419170534_create_items.rb
@@ -3,8 +3,6 @@ class CreateItems < ActiveRecord::Migration[7.0]
     create_table :items do |t|
       t.string :name , unique: true
       t.float :price
-      # t.string :item_category_id
-      # nullable
       t.string :image, default: "https://res.cloudinary.com/dvqjqgqjw/image/upload/v1587242729/default_item_image_xqjqjy.png"
       t.string :description
       t.timestamps
