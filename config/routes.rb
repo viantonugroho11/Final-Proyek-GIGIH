0db9a883494ca1463371352fde4d7aa3a587881a
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-04-23 03:33:54 +0700

    fixing base layout and fixing categories views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :items
 
-  root to: 'home#index'
+  # root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
