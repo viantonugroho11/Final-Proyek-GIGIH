e3ce28d4456f005cc906ce95132febe9598f0f40
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-04-22 18:28:34 +0700

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :item_categories
   resources :categories
   resources :items
+
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
