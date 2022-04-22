9d64eca77a658104deb4a79b23974341ebccdfee
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-04-23 05:13:53 +0700

    fixing base[header] categories[_categories] layout[application] items[_item,index]

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,2 +1,4 @@
 class ItemCategory < ApplicationRecord
+  belongs_to :item, class_name: 'item', foreign_key: 'item_id'
+  belongs_to :category, class_name: 'category', foreign_key: 'category_id'
 end
